06649cb8f645e75954e5f823902395b15641a9e5
남궁현 <nkh@encoredtech.com>
2018-06-23 18:58:12 +0900

    faet: 테스트 케스트 구조 추가

---
+++ tech_bot/src/crawling/__test__/Outsider_test.py
@@ -0,0 +1,8 @@
+import unittest
+
+class outsider_test_case(unittest.TestCase):
+  def setUp(self):
+    pass
+
+  def crawling_test(self):
+    pass
---
+++ tech_bot/tests.py
@@ -0,0 +1,10 @@
+from unittest import TestLoader, TextTestRunner, TestSuite
+
+from src.crawling.__test__.Outsider_test import outsider_test_case
+
+if __name__ == "__main__":
+    loader = TestLoader()
+    suite = TestSuite(loader.loadTestsFromTestCase(outsider_test_case))
+
+    runner = TextTestRunner(verbosity=2)
+    runner.run(suite)
